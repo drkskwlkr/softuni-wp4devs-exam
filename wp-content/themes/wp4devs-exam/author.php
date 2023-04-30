b32bd71668f7e86edf97c94da3857378457905d7
drkskwlkr <drkskwlkr@users.noreply.github.com>
2023-04-30 12:32:16 +0300

    Done with Date Archive and Author pages

---
+++ wp-content/themes/wp4devs-exam/author.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+<h1><?php the_archive_title(); ?></h1>
+
+	<ul class="properties-listing">
+	<?php if ( have_posts() ) : ?>
+		<?php while( have_posts() ) : the_post(); ?>
+				<?php get_template_part( 'template-parts/property', 'item' ); ?>
+		<?php endwhile; ?>
+			<?php posts_nav_link(); ?>
+		<?php endif; ?>
+	</ul>
+	
+<?php get_footer(); ?>
