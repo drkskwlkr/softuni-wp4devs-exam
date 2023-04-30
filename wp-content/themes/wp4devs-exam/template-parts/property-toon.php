a03fe4a7ca6452f16ad405984171c37dd0831032
drkskwlkr <drkskwlkr@users.noreply.github.com>
2023-04-30 16:54:38 +0300

    Push some more files...

--- wp-content/themes/wp4devs-exam/template-parts/property-toon.php
+++ wp-content/themes/wp4devs-exam/template-parts/property-toon.php
@@ -4,14 +4,24 @@
     <div class="property-meta">
 
       <span class="meta-location">
+        <!-- Get page views -->
         <?php
           $page_views = get_post_meta( $post->ID, 'page_load_counter', true ) ;
           if ( empty ( $page_views ) ) {
             $page_views = 1 ;
           }
         ?>
+         
+        <!-- Get page likes -->
+        <?php
+          $page_likes = get_post_meta( $post->ID, 'page_like_counter', true ) ;
 
-        <?php the_title(); ?> has <strong><?php echo $page_views ; ?></strong> page views and <strong><span id="likes-counter">333</span></strong> likes so far.
+          if ( empty ( $page_likes ) ) {
+            $page_likes = 1 ;
+          }
+        ?>
+
+        <?php the_title(); ?> has <strong><?php echo $page_views ; ?></strong> page views and <strong><span id="likes-counter"><?php echo $page_likes ;?></span></strong> likes so far.
       </span>
       <span class="meta-total-area">
       </span>
