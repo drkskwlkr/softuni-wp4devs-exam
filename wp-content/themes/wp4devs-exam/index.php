0d228003ac3c8f8259b8338ed52a25ab9e57c178
drkskwlkr <drkskwlkr@users.noreply.github.com>
2023-04-30 10:08:45 +0300

    Updated index.php to separate footer

--- wp-content/themes/wp4devs-exam/index.php
+++ wp-content/themes/wp4devs-exam/index.php
@@ -42,14 +42,4 @@
 			</li>
 		</ul>
 		
-		<footer class="site-footer">
-			<p>&copy; Copyright 2023 | Developer links: 
-				<a href="/edits.html">Edits</a>,
-				<a href="/index.html">Home</a>,
-				<a href="/single.html">Single</a>
-			</p>
-		</footer>
-	</div>
-
-</body>
-</html>
+		<?php get_footer(); ?>
