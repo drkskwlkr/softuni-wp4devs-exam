7f2b7f74fe8dfd2a30c48a96a74d2ac7ad47e27b
drkskwlkr <drkskwlkr@users.noreply.github.com>
2023-04-30 12:32:57 +0300

    Fixed author archive

--- wp-content/themes/wp4devs-exam/author.php
+++ wp-content/themes/wp4devs-exam/author.php
@@ -1,7 +1,14 @@
 <?php get_header(); ?>
 
-<h1><?php the_archive_title(); ?></h1>
+<h1><?php echo __( 'Author page for ', 'softuni' ) . get_the_author_meta( 'user_firstname' ) . ' ' . get_the_author_meta( 'user_lastname' ) ; ?></h1>
 
+<div class="box"><?php
+	$avatar = get_avatar_url( get_the_author_meta( 'user_email' ) ) ;
+	echo '<img src="' . $avatar . '" />' ;
+	$description = get_the_author_meta( 'description' ) ;
+	echo '<p>' . $description . '</p>' ;
+?></div><br />
+	
 	<ul class="properties-listing">
 	<?php if ( have_posts() ) : ?>
 		<?php while( have_posts() ) : the_post(); ?>
