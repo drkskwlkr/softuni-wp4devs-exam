dd464b30d2c8bc1a5fd29ca9fbc378487302f9d1
drkskwlkr <drkskwlkr@users.noreply.github.com>
2023-04-30 09:56:33 +0300

    Add missing source files

---
+++ wp-content/themes/wp4devs-exam/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="<?php bloginfo( 'charset' ); ?>" />
+  <link rel="profile" href="//gmpg.org/xfn/11" />
+  <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  
+  <title><?php bloginfo( 'title' ); ?></title>
+
+	<!--link rel="stylesheet" href="./css/master.css"-->
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri() ; ?>/css/master.css">
+	<link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet">
+
+  <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+	<div class="site-wrapper">
+		<header class="site-header">
+			<!--h1 class="site-title"><a href="#">Properties Offers</a></h1-->
+			<h1 class="site-title"><a href="<?php echo esc_url( get_home_url() ); ?>"><?php bloginfo( 'title' ); ?></a></h1>
+		</header>
