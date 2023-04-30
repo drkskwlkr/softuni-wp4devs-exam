cdc3dae14993ebb7b7cd2a4cfae712f0bef9ca67
drkskwlkr <drkskwlkr@users.noreply.github.com>
2023-04-30 10:08:10 +0300

    Create footer.php, add dynamic year

---
+++ wp-content/themes/wp4devs-exam/footer.php
@@ -0,0 +1,12 @@
+<footer class="site-footer">
+			<p>&copy; Copyright <?php echo date('Y') ; ?> | Developer links: 
+				<a href="/edits.html">Edits</a>,
+				<a href="/index.html">Home</a>,
+				<a href="/single.html">Single</a>
+			</p>
+      <?php wp_footer(); ?>
+		</footer>
+	</div>
+
+</body>
+</html>
