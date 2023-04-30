2a8f4400653dec4a7c1402014ae24ad2a77906f4
drkskwlkr <drkskwlkr@users.noreply.github.com>
2023-04-30 14:26:00 +0300

    CPT creation seems to work

--- wp-content/plugins/wp4devs-exam/wp4devs-exam.php
+++ wp-content/plugins/wp4devs-exam/wp4devs-exam.php
@@ -1,4 +1,12 @@
 <?php
+/**
+* Plugin Name: WP for Developers (March 2023 Exam)
+* Plugin URI: https://softuni.bg
+* Description: This plugin will register a Cartoon post type and allow people to vote for their favorite toon.
+* Version: 0.1
+* Author: Ivan Arnaudov
+**/
+
 
 /**
  * The main file of the plugin will be very minimal.
