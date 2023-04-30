c27de38fd6acda8ea1b6343793e5296c47daf72d
drkskwlkr <drkskwlkr@users.noreply.github.com>
2023-04-30 15:29:09 +0300

    Almost finished Toons CPT design.

--- wp-content/plugins/wp4devs-exam/cpt-cartoons.php
+++ wp-content/plugins/wp4devs-exam/cpt-cartoons.php
@@ -11,7 +11,7 @@
  *
  * @see get_post_type_labels() for label keys.
  */
-function softuni_Toons_cpt() {
+function softuni_toons_cpt() {
 	$labels = array(
 		'name'                  => _x( 'Toons', 'Post type general name', 'softuni' ),
 		'singular_name'         => _x( 'Toon', 'Post type singular name', 'softuni' ),
@@ -23,7 +23,7 @@ function softuni_Toons_cpt() {
 		'edit_item'             => __( 'Edit Toon', 'softuni' ),
 		'view_item'             => __( 'View Toon', 'softuni' ),
 		'all_items'             => __( 'All Toons', 'softuni' ),
-	);
+	) ;
 
 	$args = array(
 		'labels'             => $labels,
@@ -37,13 +37,44 @@ function softuni_Toons_cpt() {
 		'has_archive'        => true,
 		'hierarchical'       => false,
 		'menu_position'      => null,
-		'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'revisions' ),
+		'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'revisions', 'custom-fields' ),
     'show_in_rest'       => true,
     'menu_icon'          => 'dashicons-nametag'
-	);
+	) ;
 
-	register_post_type( 'Toon', $args );
+  register_post_type( 'Toon', $args ) ;
 
-    flush_rewrite_rules();
+  flush_rewrite_rules() ;
 }
-add_action( 'init', 'softuni_Toons_cpt' );
+add_action( 'init', 'softuni_Toons_cpt' ) ;
+
+/**
+ * Register a custom taxonomy (race/origin)
+ */
+function softuni_toons_field_taxonomy() {
+  $labels = array(
+  'name'              => _x( 'Toon Origin', 'taxonomy general name', 'softuni' ),
+  'singular_name'     => _x( 'Toon Origin', 'taxonomy singular name', 'softuni' ),
+  'search_items'      => __( 'Search by origin', 'softuni' ),
+  'all_items'         => __( 'All origins', 'softuni' ),
+  'parent_item'       => __( 'Parent Field', 'softuni' ),
+  'parent_item_colon' => __( 'Parent Field:', 'softuni' ),
+  'edit_item'         => __( 'Edit Field', 'softuni' ),
+  'update_item'       => __( 'Update Field', 'softuni' ),
+  'add_new_item'      => __( 'Add New Origin', 'softuni' ),
+  'new_item_name'     => __( 'New Origin Name', 'softuni' ),
+  'menu_name'         => __( 'Toon Origin', 'softuni' ),
+  ) ;
+
+  $args = array(
+  'hierarchical'      => false,
+  'labels'            => $labels,
+  'show_ui'           => true,
+  'show_admin_column' => true,
+  'query_var'         => true,
+  'show_in_rest'      => true
+  ) ;
+
+register_taxonomy( 'origin', 'toon', $args ) ;
+}
+add_action( 'init', 'softuni_toons_field_taxonomy' ) ;
--- wp-content/themes/wp4devs-exam/single-toon.php
+++ wp-content/themes/wp4devs-exam/single-toon.php
@@ -1,105 +1,66 @@
 <?php get_header(); ?>
 
-		<H1>SINGLETOON</H1>
-
-		<div class="property-single">
+<div class="property-single">
 			<main class="property-main">
 				<div class="property-card">
 					<div class="property-primary">
 						<header class="property-header">
-							<!--h2 class="property-title"><a href="#">Two-bedroom apartment</a></h2-->
+							<div class="property-image-box property-image-box-single">
+							<?php
+								if ( has_post_thumbnail() ) {
+									the_post_thumbnail();
+								} else {
+									echo '<img src="' . get_template_directory_uri() . '/images/bedroom.jpg" alt="property image">' ;
+								}
+							?>
+							</div>
+							<br />
 							<h1 class="property-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-							<div class="property-meta">
-								<span class="meta-location">
-									<?php
-										if( null !== get_post_custom_values( 'Location' )) {
-											echo get_post_custom_values( 'Location' )[0] ;
-										} else {
-											echo __( 'Location not entered', 'softuni' ) ;
-										}
-									?>
-								</span>
-								<span class="meta-total-area">
-									<?php
-										if( null !== get_post_custom_values( 'Total Area' )) {
-											echo __( 'Total area: ', 'softuni' ) . get_post_custom_values( 'Total Area' )[0] . ' sq.m' ;
-										} else {
-											echo __( 'Total area not entered', 'softuni' ) ;
-										}
-									?>
-								</span><br>
-								<span class="meta-price">
-									<?php
-										if( null !== get_post_custom_values( 'Asking Price' )) {
-											echo __( 'Asking Price: ', 'softuni' ) . '€' . number_format( get_post_custom_values( 'Asking Price' )[0], 2, '.', ',') ;
-										} else {
-											echo __( 'Asking price not entered', 'softuni' ) ;
-										}
-									?>
-								</span>
-							</div>
+								<div class="property-body">
+									<?php the_content() ; ?>
+								</div>
 
-							<div class="property-details grid">
-								<div class="property-details-card">
-									<div class="property-details-card-title">
-										<h3>Rooms</h3>
+								<div class="property-details grid">
+									<div class="property-details-card">
+										<div class="property-details-card-title">
+											<h3><?php echo __('Unique facts about this character', 'softuni') ; ?></h3>
+										</div>
+										<div class="property-details-card-body">
+											<ul>
+												<?php
+												$specialties = get_post_custom_values('Facts about this character')[0] ;
+												//var_dump($specialties) ;
+
+												if ( null == $specialties) {
+													echo __('We have no facts about this toon', 'softuni') ;
+												} else {
+													$list = explode( '|', $specialties) ;
+													foreach ($list as $list_item) {
+														echo '<li> » ' . esc_html($list_item) . '</li>';
+													}
+												}
+												?>
+											</ul>
+										</div>
 									</div>
-									<div class="property-details-card-body">
-										<ul>
-											<li>2 Bedrooms</li>
-											<li>1 Bathroom</li>
-											<li>1 Living room</li>
-											<li>Separated kitchen</li>
-										</ul>
+									<div class="property-details-card">
+										<div class="property-details-card-title">
+											<h3>Votes</h3>
+										</div>
+										<div class="property-details-card-body">
+											<h3 id="toon-votes">333 likes</h3>
+											<div><a href="#" class="button button-wide"><?php echo __('Like this Toon', 'softuni') ; ?></a></div>
+										</div>
 									</div>
 								</div>
-								<div class="property-details-card">
-									<div class="property-details-card-title">
-										<h3>Additional Details</h3>
-									</div>
-									<div class="property-details-card-body">
-										<ul>
-											<li>Floor: 6</li>
-											<li>Elevator/Lift</li>
-											<li>Brick-built</li>
-											<li>Electricity</li>
-											<li>Water Supply</li>
-											<li>Heating</li>
-										</ul>
-									</div>
-								</div>
-							</div>
 				
 						</header>
-
-						<div class="property-body">
-							<p>Real estate agency "New Home" presents you a two-room apartment in a newly built building in Ovcha Kupel. With quick access to the Ring Road, close to public transport stops, shops and restaurants, a kindergarten.</p>
-							<p>The apartment has an entrance hall, living room with kitchenette, bedroom, bathroom with toilet, terrace.</p>
-							<p>The exposure is east/north.<br>There is an opportunity to purchase a garage in the building.</p>
-							<p>The building will be completed with luxury common areas, staircases and a lobby. Commercial areas include a restaurant, gym, a beauty salon, a shop, and a pharmacy.</p>
-						</div>
 					</div>
 				</div>
 			</main>
-			<aside class="property-secondary">
-				<div class="property-image property-image-single">
-					<div class="property-image-box property-image-box-single">
-						<?php
-							if ( has_post_thumbnail() ) {
-								the_post_thumbnail();
-							} else {
-								echo '<img src="' . get_template_directory_uri() . '/images/bedroom.jpg" alt="property image">' ;
-							}
-      			?>
-						<!--img src="images/bedroom.jpg" alt="property image"-->
-					</div>
-				</div>
-				<?php // TODO: Add Likes Counter ?>
-				<a href="#" class="button button-wide">Like the property</a>
-			</aside>
 		</div>
 
-		<h2 class="section-heading">Other similar properties:</h2>
+		<h2 class="section-heading"><?php echo __('Other toons:', 'softuni') ; ?></h2>
 
 		<ul class="properties-listing">
 		<?php
