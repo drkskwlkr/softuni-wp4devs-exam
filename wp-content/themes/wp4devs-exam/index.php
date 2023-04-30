1d366dc6306624e159236fe1c43276ac34cfe18b
drkskwlkr <drkskwlkr@users.noreply.github.com>
2023-04-30 09:54:36 +0300

    Separated html header stuff into separate file header.php

--- wp-content/themes/wp4devs-exam/index.php
+++ wp-content/themes/wp4devs-exam/index.php
@@ -1,21 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Properties</title>
-	<link rel="preconnect" href="https://fonts.gstatic.com">
-
-	<!--link rel="stylesheet" href="./css/master.css"-->
-	<link rel="stylesheet" href="<?php echo get_template_directory_uri() ; ?>/css/master.css">
-	<link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet">
-</head>
-<body>
-	<div class="site-wrapper">
-		<header class="site-header">
-			<h1 class="site-title"><a href="#">Properties Offers</a></h1>
-		</header>
+<?php get_header(); ?>
 
 		<ul class="properties-listing">
 			<li class="property-card">
